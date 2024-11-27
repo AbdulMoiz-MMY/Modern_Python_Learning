134b6cfa77f92de9765fc22bf09f5c9c0279281f
Abdul Moiz Bhatti <abdulmoizbhatti17@gmail.com>
2024-11-27 10:57:32 +0500

    practice queation

---
+++ step_1/forthprogram.py
@@ -0,0 +1,8 @@
+## WAP to input 2 int numbers, a and b.
+## print True if a is greater than or equal to b.if not print False.
+
+print("Perform the value is greater than or less than")
+a = int(input("Enter the 1st number: "))
+b = int(input("Enter the 2nd number: "))
+
+print(a >= b)
