5a79c2bd862c8200828e5efb2d42bc1d86221373
Abdul Moiz Bhatti <abdulmoizbhatti17@gmail.com>
2024-12-01 08:06:42 +0500

    add commit

--- Step_4/Dicyionary.py
+++ Step_4/Dicyionary.py
@@ -1,8 +1,53 @@
-dic_1 = {
+info = {
+    "key": "Value",
     "name": "Abdul Moiz",
-    "class": "3-year",
-    "city": "Lahore"
-
+    "subject": ["Python","C","Java"],
+    "topics" : ("dict","set"),
+    "age": 19,
+    12.4: 1.5
 }
 
-print(dic_1)
+print(type(info))
+
+print(info["name"])
+print(info["subject"])
+print(info["topics"])
+print(info["age"])
+print(info[12.4])
+
+
+info["name"] = "Bhatti"
+
+print(info)
+
+info["status"] = True
+
+print(info)
+
+
+student ={
+    "name" : "Abdul Moiz",
+    "subject" : {
+        "phy":46,
+        "chem":46,
+        "math" : 87
+    }
+}
+
+
+print(student["subject"]["phy"])
+
+print(len(list(student.keys())))
+
+print(list(student.values()))
+
+pairs = list(student.items())
+
+print(pairs[0])
+
+
+print(student.get("name"))  #Error
+print(student["name"])    # no Error -> None
+
+student.update({"city": "Lahore  "})
+
